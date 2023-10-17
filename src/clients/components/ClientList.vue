e291a0bcf91cb6c45ca7c8191ae4be012d11e9dd
Cristina Aguilar Rodriguez <aguilar.c@ugto.mx>
2023-10-17 12:20:18 -0600

    Add - Se añadieron componentes de ClientList y PaginationNumbers

---
+++ src/clients/components/ClientList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { Client } from '@/clients/interfaces/client'
+
+interface Props {
+    clients: Client[]
+}
+
+const props = defineProps<Props>()
+ 
+
+</script>
+
+<template>
+    <!-- <span>{{ props }}</span> -->
+    <ul>
+        <li v-for="client of clients" :key="client.id">
+            <RouterLink :to="{
+                name: 'client-id',
+                params: { id: client.id },
+            }"> {{  client.name }}
+            </RouterLink>
+        </li>
+    </ul>
+</template>
+
+
+<style scoped>
+
+</style>
