c345d0a32444f4f149d78c21e4d6a6abe6b02864
Cristina Aguilar Rodriguez <aguilar.c@ugto.mx>
2023-10-17 12:21:42 -0600

    Add - Se añadio pagina de ListPage para mostrar los clientes

--- src/clients/pages/ListPage.vue
+++ src/clients/pages/ListPage.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
+import ClientList from '../components/ClientList.vue';
+import PaginationNumbers from '../components/PaginationNumbers.vue';
+import useClients from '../composables/useClients';
+import LoadingModal from '@/shared/LoadingModal.vue'
+
+const { isLoading, clients, currentPage, totalPages, getPage } = useClients();
+
 
 </script>
 
 <template>
     <div>
         <h1>Listado de clientes</h1>
+
+        <!-- Client List -->
+        <ClientList :clients="clients"/>
+        <!-- PaginationNumber -->
+        <PaginationNumbers 
+            :current-page="currentPage"
+            :total-pages="totalPages"
+            @page-changed="getPage"
+        />
+        <!-- LoadingModal -->
+        <LoadingModal v-if="isLoading"/>
     </div>
 </template>
 
